1036ed1296fe3b890b788128194e6ef9ded8bf92
Menu Temesgen <menutemesgen@gmail.com>
2025-07-05 21:11:34 +0300

    -- i added the 2-lazy_paginate file.

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,32 @@
+import mysql.connector
+
+def paginate_users(page_size, offset):
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='Nafiad',
+        password='Nafiad@mysql12'
+        database='ALX_prodev'
+    )
+
+    cursor = connection.cursor(dictionary=True)
+
+    query = "SELECT * FROM user_data LIMIT %s OFFSET %s"
+    cursor.execute(query, (page_size, offset))
+
+    rows = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return rows
+
+
+def lazy_paginate(page_size):
+    offset = 0
+
+    while True:
+        page = paginate_users(page_size, offset)
+        if not page:
+            break
+        yield page
+        offset += page_size
