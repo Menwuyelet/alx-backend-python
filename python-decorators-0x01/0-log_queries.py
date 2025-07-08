725d82fe6d6e000b67598081f1c890c513a61577
Menu Temesgen <menutemesgen@gmail.com>
2025-07-08 10:47:07 +0300

    -- i completed the 0-log_queries task and added it to the directory.

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,32 @@
+import sqlite3
+import functools
+import logging
+import time
+#### decorator to lof SQL queries
+
+""" YOUR CODE GOES HERE"""
+
+logging.basicConfig(level = logging.INFO, format = '%(asctime)s - %(message)s')
+
+def log_queries(func):
+    functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        logging.info(f"Execution of query function '{func.__name__}' started.")
+        logging.info(f"args = {args}, kwargs = {kwargs}")
+        result = func(*args, **kwargs)  
+        end_time = time.time()
+        logging.info(f"Execution of query function '{func.__name__}'' ended. execution time: {end_time - start_time}s")    
+        return result
+    return wrapper
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
