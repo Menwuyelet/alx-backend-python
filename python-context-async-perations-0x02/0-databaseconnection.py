f77f971e8e10dc7a6f6610852d01a6633b02c14c
Menu Temesgen <menutemesgen@gmail.com>
2025-07-10 10:17:17 +0300

    -- updated the 0-databaseconnection to meet the task requirement.

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -1,19 +1,32 @@
+import sqlite3 
+
 class FileOpener:
-    def __init__(self, filename, mode):
-        self.filename = filename
-        self.mode = mode
-        self.file = None
-    
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+
     def __enter__(self):
-        self.file = open(self.filename, self.mode)
-        return self.file
-    
+        self.conn = sqlite3.connect('users.db')
+        return self.conn
     def __exit__(self, exc_type, exc_value, traceback):
-        if self.file:
-            self.file.close()
+        if self.conn:
+            if exc_type:
+                print("An error occurred. Rolling back the db.")
+                self.conn.rollback()
+            else:
+                print("Committing transaction")
+                self.conn.commit()
 
+            self.conn.close()
 
-with FileOpener("file.txt", "w") as f:
-    f.write("Test for the implementation of context manager.")
-
+with FileOpener("users") as db:
+    cursor = db.cursor()
+    # cursor.execute("CREATE TABLE IF NOT EXISTS users (" \
+    # "id INTEGER PRIMARY KEY AUTOINCREMENT," \
+    # "name TEXT NOT NULL," \
+    # "email TEXT UNIQUE NOT NULL," \
+    # "password TEXT NOT NULL)" )
+    # cursor.execute("INSERT INTO users (id, name, email, password)" \
+    # "VALUES (1, 'nafiad', 'ex@mail.com', '123we;')")
+    cursor.execute("SELECT * FROM users")
         
