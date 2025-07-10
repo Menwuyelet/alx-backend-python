bd3d90e907d0f469eb4012793b97e16939d47788
Menu Temesgen <menutemesgen@gmail.com>
2025-07-10 11:05:09 +0300

    -- i updated the 3-concurrent to meet the checker requirment.

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -6,21 +6,25 @@ async def async_fetch_users():
     async with aiosqlite.connect("users.db") as db:
         async with db.execute("SELECT * FROM users") as cursor:
             rows = await cursor.fetchall()
-            for row in rows:
-                print(row)
-
+            return rows
+        
 async def async_fetch_older_users():
     async with aiosqlite.connect("users.db") as db:
         async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
             rows = await cursor.fetchall()
-            for row in rows:
-                print(row)
+            return rows
 
 async def fetch_concurrently():
-    await asyncio.gather(
+    all_users, older_users = await asyncio.gather(
         async_fetch_users(),
         async_fetch_older_users()
     )
+
+    for row in all_users:
+        print(row)
+    
+    for row in older_users:
+        print(row)
 
 if __name__ == "__main__":
     asyncio.run(fetch_concurrently())
