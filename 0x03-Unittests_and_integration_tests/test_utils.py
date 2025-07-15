3c1a62c3b901423e79df190f26f139590f40417f
Menu Temesgen <menutemesgen@gmail.com>
2025-07-15 14:43:15 +0300

    -- i added a test case to test the unavailable paths.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,6 +23,21 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(
+        self,
+        nested_map: dict,
+        path: tuple,
+        expected: object
+    ) -> None:
+        """
+        Test that access_nested_map raises "KeyError" for the unavailable paths
+        """
+        self.assertRaises(expected, access_nested_map, nested_map, path)
+
 
 if __name__ == '__main__':
     unittest.main()
