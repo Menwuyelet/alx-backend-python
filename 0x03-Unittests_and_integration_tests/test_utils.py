4613d91760a3d86531c4301a78bb29567b478946
Menu Temesgen <menutemesgen@gmail.com>
2025-07-16 10:58:42 +0300

    -- i added tests to test get_json function to test_utils.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Unit tests for access_nested_map function from utils module."""
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from parameterized import parameterized
 import unittest
+from unittest import mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -37,6 +38,31 @@ class TestAccessNestedMap(unittest.TestCase):
         Test that access_nested_map raises "KeyError" for the unavailable paths
         """
         self.assertRaises(expected, access_nested_map, nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test case for get_json function from utils module."""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(
+        self,
+        url: str,
+        expected: dict
+    ) -> None:
+        """
+        Test that get_json method returns expected result for the given urls
+        with out calling the actual http calls.
+        """
+        with mock.patch('utils.requests.get') as mocked:
+            mock_response = mock.Mock()
+            mock_response.json.return_value = expected
+            mocked.return_value = mock_response
+
+            result = get_json(url)
+            mocked.assert_called_once_with(url)
+            self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
