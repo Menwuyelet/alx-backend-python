a97b47e1ff735b262baa3f93a571045dc95d31ee
Menu Temesgen <menutemesgen@gmail.com>
2025-07-17 16:21:22 +0300

    -- i added the TestIntegrationGithubOrgClient class to test public_repos from client module.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Unit tests for GithubOrgClient"""
 import unittest
-from unittest import mock
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
+from parameterized import parameterized_class
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -40,7 +41,7 @@ class TestGithubOrgClient(unittest.TestCase):
         with patch.object(
             GithubOrgClient,
             'org',
-            new_callable=mock.PropertyMock
+            new_callable=PropertyMock
         ) as mock_org:
             mock_org.return_value = mock_payload
             client = GithubOrgClient("testorg")
@@ -65,7 +66,7 @@ class TestGithubOrgClient(unittest.TestCase):
         with patch.object(
             GithubOrgClient,
             "_public_repos_url",
-            new_callable=mock.PropertyMock
+            new_callable=PropertyMock
         ) as mock_url:
             mock_url.return_value = "https://api.github.com/orgs/testorg/repos"
 
@@ -93,6 +94,64 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         result = GithubOrgClient.has_license(repo, license_key)
         self.assertEqual(result, expected)
+
+
+@parameterized_class([
+    {
+        "org_payload": org,
+        "repos_payload": repos,
+        "expected_repos": expected,
+        "apache2_repos": apache2
+    }
+    for org, repos, expected, apache2 in TEST_PAYLOAD
+])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """
+    Integration test class using real fixture payload to test public_repos
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Starts patching requests.get"""
+        cls.get_patcher = patch("requests.get")
+        mocked_get = cls.get_patcher.start()
+
+        # Side effect to mock requests.get().json()
+        def side_effect(url):
+            mock_resp = MagicMock()
+            if url == GithubOrgClient.ORG_URL.format(org="google"):
+                mock_resp.json.return_value = cls.org_payload
+            elif url == cls.org_payload["repos_url"]:
+                mock_resp.json.return_value = cls.repos_payload
+            else:
+                mock_resp.json.return_value = None
+            return mock_resp
+
+        mocked_get.side_effect = side_effect
+
+    @classmethod
+    def tearDownClass(cls):
+        """Stops patching"""
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """
+        Tests the GithubOrgClient.public_repos returns expected repo names
+        """
+        client = GithubOrgClient("google")
+        self.assertEqual(client.public_repos(), self.expected_repos)
+
+    def test_public_repos_with_license(self):
+        """
+        Tests the GithubOrgClient.public_repos filters by 'apache-2.0' license
+        """
+        client = GithubOrgClient("google")
+        self.assertEqual(
+            client.public_repos(
+                license="apache-2.0"
+            ),
+            self.apache2_repos
+        )
 
 
 if __name__ == "__main__":
