ae4faf84048f1eaa17b14b3cd71a772775417f4a
Menu Temesgen <menutemesgen@gmail.com>
2025-07-19 18:50:37 +0300

    -- i added serializers for all of the models in chats app.

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,33 @@
+from .models import User, Conversation, Message
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    class Meta:
+        model = User
+        fields = ['user_id', 'email', 'first_name', 'last_name', 'phone_number', 'password', 'role']
+        read_only_fields = ['user_id']
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(source='sender_id', read_only=True)
+    class Meta:
+        model = Message
+        fields = ['message_id', 'sender_id', 'message_body', 'sent_at', 'conversation_id']
+        read_only_fields = ['message_id', 'sent_at']
+    
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(source='participants_id', read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants_id', 'created_at']
+        read_only_fields = ['conversation_id', 'created_at']
+
+
