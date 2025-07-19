62fec0d98643a964634d27f4799cdd4bab0e96b9
Menu Temesgen <menutemesgen@gmail.com>
2025-07-19 19:28:56 +0300

    -- edited import statement to meet checker requirement.

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,7 +1,7 @@
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet, UserCreateViewSet
 from django.urls import path, include
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 router.register(r'users', UserCreateViewSet, basename='user')
