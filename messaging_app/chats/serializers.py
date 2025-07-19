cdacd50bfd800c9cb1fda044007b9a1025668c65
Menwuyelet <menutemesgen@gmail.com>
2025-07-19 19:00:36 +0300

    Update serializers.py to include email validation and full name generation.

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,10 +3,20 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
+    full_name = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = ['user_id', 'email', 'first_name', 'last_name', 'phone_number', 'password', 'role']
         read_only_fields = ['user_id']
+    
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("The email you provided is already in use. Please use different email.")
+        return value
     
     def create(self, validated_data):
         password = validated_data.pop('password')
