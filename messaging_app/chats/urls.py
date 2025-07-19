ba61390c8e9709b5f2ee3f7b4eb7ec79b18a95dd
Menu Temesgen <menutemesgen@gmail.com>
2025-07-19 19:36:29 +0300

    -- i added end points to make the viewsets accessible.

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,13 @@
 from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet, UserCreateViewSet
 from django.urls import path, include
+from rest_framework_nested.routers import NestedDefaultRouter
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
-router.register(r'users', UserCreateViewSet, basename='user')
+conversation_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversation_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(conversation_router.urls)),
+]
