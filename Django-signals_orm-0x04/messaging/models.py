af7c21bd28d825f4660a5113f3e0f4fe3d7a6415
Menu Temesgen <menutemesgen@gmail.com>
2025-07-30 10:57:54 +0300

    -- i created the messaging app and inside the app. -- inside that app i implemented message and notfication models and signals to automate notfication creation.

---
+++ Django-signals_orm-0x04/messaging/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"From {self.sender} to {self.receiver} at {self.timestamp}"
+    
+class Notification(models.Model):
+    messages = models.OneToOneField(Message, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    
+    def __str__(self):
+        return f"Notification for {self.user} from message {self.message.id}"
