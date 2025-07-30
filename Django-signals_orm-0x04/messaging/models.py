871e72de7b85c712f90c88cc5cb645b3bf231a2f
Menu Temesgen <menutemesgen@gmail.com>
2025-07-30 11:23:23 +0300

    -- i added the message_history model to store the changed message contents and added a signal to automate the creation

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -6,6 +6,7 @@ class Message(models.Model):
     receiver = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='received_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
 
     def __str__(self):
         return f"From {self.sender} to {self.receiver} at {self.timestamp}"
@@ -17,3 +18,12 @@ class Notification(models.Model):
     
     def __str__(self):
         return f"Notification for {self.user} from message {self.message.id}"
+    
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='message')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Message id {self.message.id} - updated at {self.edited_at}"
+    
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,8 +1,16 @@
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(message=instance, user=instance.receiver)
+
+@receiver(pre_save, sender=Message)
+def create_message_history(sender, instance, **kwargs):
+    if instance.pk:
+        old_instance = Message.objects.get(pk=instance.pk)
+        if old_instance.content != instance.content:
+            MessageHistory.objects.create(message=instance, old_content=old_instance.content)
+            instance.edited = True
