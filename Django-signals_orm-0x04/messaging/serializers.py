756985d4d8aff216d926e244e4d36df8ebf52232
Menu Temesgen <menutemesgen@gmail.com>
2025-07-31 09:33:22 +0300

    -- i added a parent field to reference to self to allow message replies and implemented views and serializers to make it available.

--- Django-signals_orm-0x04/messaging/serializers.py
+++ Django-signals_orm-0x04/messaging/serializers.py
@@ -1,8 +1,20 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Message
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
     
+class MessageSerializer(serializers.ModelSerializer):
+    replies = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'receiver', 'content', 'timestamp', 'edited', 'parent_message', 'replies']
+        read_only_fields = ['id', 'sender','timestamp', 'edited']
+
+    def get_replies(self, obj):
+        queryset = obj.replies.all().select_related('parent_message').prefetch_related('replies')
+        return MessageSerializer(queryset, many=True, context=self.context).data
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -2,9 +2,29 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import Prefetch
+from .models import Message
+from .serializers import MessageSerializer
 
 @api_view(['DELETE'])
 @permission_classes([IsAuthenticated])
 def delete_user(request):
     request.user.delete()
     return Response({"detail": "User deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+@api_view(['GET'])
+def threaded_messages_view(request):
+    messages = Message.objects.filter(parent_message__isnull=True)\
+        .select_related('sender', 'receiver')\
+        .prefetch_related(
+            Prefetch(
+                'replies',
+                queryset=Message.objects.select_related('sender', 'receiver', 'parent_message').prefetch_related('replies')
+            )
+        )
+
+    serializer = MessageSerializer(messages, many=True, context={'request': request})
+    return Response(serializer.data)
