fe734e6812ee27ffed7d7b61b50015003c5da72c
Menu Temesgen <menutemesgen@gmail.com>
2025-08-09 09:20:07 +0300

    Set up MySQL database with Docker and update requirements; add .env to .gitignore

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -103,11 +103,15 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+import os
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE', 'messaging_db'),
+        'USER': os.getenv('MYSQL_USER', 'messaging_user'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', 'strong_password_here'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': '3306',
     }
 }
 
